3034d0114555c13b611cee5d5a8fbb08fffc5768
Prateeksingchn <114944321+Prateeksingchn@users.noreply.github.com>
2024-03-11 14:22:40 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+// script.js
+const passwordBox = document.getElementById("Password");
+const length = 16;
+
+const upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerCase = "abcdefghijklmnopqrstuvwxyz";
+const number = "0123456789";
+const symbol = "@#$%^&*()_+~|;[]></-=";
+
+const allChars = upperCase + lowerCase + number + symbol; 
+
+function createPassword(){
+    let password = "";
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += number[Math.floor(Math.random() * number.length)];
+    password += symbol[Math.floor(Math.random() * symbol.length)];
+
+    while (length > password.length) {
+        password += allChars[Math.floor(Math.random() * allChars.length)];
+    }
+    passwordBox.value = password;
+}
+
+function copyPassword() {
+    passwordBox.select(); // Select the text in the input
+    document.execCommand("copy"); // Copy the selected text
+    // alert("Password copied to clipboard!");
+}
+
